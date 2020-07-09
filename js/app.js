ec17c917bc317408a6296621c21440c6330e32d7
malloryclark93 <mallory_clark@live.com>
2020-07-08 23:16:44 -0600

    please check your numbers alert added

--- js/app.js
+++ js/app.js
@@ -29,11 +29,28 @@ function calculateResults(e){
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
   } else { 
-    console.log('Please check your numbers');
+    // console.log('Please check your numbers');
+    showError('Please check your numbers');
   }
 
 e.preventDefault();
 }
 
+// Show error
+function showError(error){
+  // create a div
+  const errorDiv = document.createElement('div');
 
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+  // Add Class
+  errorDiv.className = 'alert alert-danger';
 
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading 
+  card.insertBefore(errorDiv, heading);
+
+}
