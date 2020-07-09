33142ac3921a132e0a839df63007287aea1d698c
malloryclark93 <mallory_clark@live.com>
2020-07-09 00:00:21 -0600

    final changes-working calc

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 // Listen for submit 
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // hide results 
+  document.getElementById('results').style.display = 'none';
+  // show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate results
-function calculateResults(e){ 
+function calculateResults(){ 
     console.log('calculating..');
   // UI variables
   const amount = document.getElementById('amount');
@@ -28,17 +37,30 @@ function calculateResults(e){
     //toFixed sets the number of decimal points
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+    // Hide loader 
+    document.getElementById('loading').style.display = 'none';
+
+
   } else { 
     // console.log('Please check your numbers');
     showError('Please check your numbers');
   }
 
-e.preventDefault();
+
 }
 
 // Show error
 function showError(error){
-  // create a div
+
+   // Show results
+   document.getElementById('results').style.display = 'none';
+   // Hide loader 
+   document.getElementById('loading').style.display = 'none';
+  
+   // create a div
   const errorDiv = document.createElement('div');
 
   // Get elements
@@ -62,3 +84,4 @@ function showError(error){
 function clearError(){
   document.querySelector('.alert').remove();
 }
+
