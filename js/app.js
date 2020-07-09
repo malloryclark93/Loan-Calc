04cd64e16e04ebeee52ea1250972cc722bba5be9
malloryclark93 <mallory_clark@live.com>
2020-07-08 22:59:00 -0600

    working code for getting total

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// Listen for submit 
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate results
+function calculateResults(e){ 
+    console.log('calculating..');
+  // UI variables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+
+  // Value of calculation -- as float (decimal)
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12; // 100% divided by 12 months
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute Monthly Payments
+  // Math power 
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+
+  if(isFinite(monthly)){
+    monthlyPayment.value = monthly.toFixed(2);
+    //toFixed sets the number of decimal points
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+  } else { 
+    console.log('Please check your numbers');
+  }
+
+e.preventDefault();
+}
+
+
+
